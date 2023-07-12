27f5d7a064025319c6f26da631f7da5bd89a4844
Aleksandr Sitnikov <gnomek100500@gmail.com>
2023-07-12 14:40:09 +0300

    featured: апдейт ссылок на фронте

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -99,7 +99,7 @@ export class Api {
 }
 
 const api = new Api({
-    baseUrl: 'http://api.gnomdomen.nomoredomains.work',
+    baseUrl: 'https://api.gnomdomen.nomoredomains.work',
     headers: {
         authorization: 'cf1f2fd8-5312-427b-b17f-83a02fd1127b',
         'Content-Type': 'application/json'
